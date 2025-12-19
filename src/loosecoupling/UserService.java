8b83b9d89467534d3f651e0d4466f43eb828e574
jonsi <jonsimisharwal@gmail.com>
2025-12-19 23:20:30 +0530

    Coupling demo

---
+++ src/loosecoupling/UserService.java
@@ -0,0 +1,22 @@
+package loosecoupling;
+
+
+
+public class UserService {
+
+  public NotificationService notificationservice;
+  public UserService(){
+
+  }
+  public UserService(NotificationService notificationservice){
+          this.notificationservice=notificationservice;
+  }
+   public void notifyUser(String message){
+
+       notificationservice.send("Notification hello");
+   }
+
+    public void setNotificationservice(NotificationService notificationservice) {
+        this.notificationservice = notificationservice;
+    }
+}
